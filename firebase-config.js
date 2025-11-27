5c92669311e5831c12f8ab1522569c995c19f35a
Bora Kol <borakol07@gmail.com>
2025-11-27 23:02:29 +0300

    authentication service set on firebase

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyBcSu9EacGoYq5D6Y_xVQDskMi1yc9mtnI",
+    authDomain: "pacman-game-c016b.firebaseapp.com",
+    projectId: "pacman-game-c016b",
+    storageBucket: "pacman-game-c016b.firebasestorage.app",
+    messagingSenderId: "46312322428",
+    appId: "1:46312322428:web:9901380af24358dff020f9",
+    measurementId: "G-HWK1X24WPP"
+};
